62a81c12ac7cdafcdf5b77833aac0790adde050a
AJP <allenatmc@gmail.com>
2016-03-01 11:44:52 -0600

    add album customer and invoice api routes

---
+++ models/customer.js
@@ -0,0 +1,35 @@
+'use strict';
+
+module.exports = function(sequelize, DataTypes) {
+  var Customer = sequelize.define('Customer', {
+    CustomerId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    SupportRepId: DataTypes.INTEGER,
+    FirstName: DataTypes.STRING,
+    LastName: DataTypes.STRING,
+    Company: DataTypes.STRING,
+    Address: DataTypes.STRING,
+    City: DataTypes.STRING,
+    State: DataTypes.STRING,
+    Country: DataTypes.STRING,
+    PostalCode: DataTypes.STRING,
+    Phone: DataTypes.STRING,
+    Fax: DataTypes.STRING,
+    Email: DataTypes.STRING
+
+  }, {
+    tableName: 'Customer',
+    timestamps: false,
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+        // Customer.belongsTo(models.Employee, {foreignKey: "SupporRepId"});
+      }
+    }
+  });
+
+  return Customer;
+};
--- server.js
+++ server.js
@@ -34,6 +34,33 @@ app.get('/playlist', (req, res) => {
   .then((data) => {  res.send(data);});
 });
 
+app.get('/invoice', (req, res) => {
+  models.Invoice.findAll({
+      attributes: {exclude: "ExcludeId"},
+      include: [{
+        model: models.Customer,
+        attributes: {exclude: "CustomerId"}
+      }]
+    })
+    .then(data => res.send(data));
+});
+
+app.get('/customer', (req, res) => {
+  models.Customer.findAll()
+  .then((data) => {  res.send(data);});
+});
+
+app.get('/album', (req, res) => {
+  models.Album.findAll({
+      attributes: {exclude: "ArtistId"},
+      include: [{
+        model: models.Artist,
+        attributes: {exclude: "ArtistId"}
+      }]
+    })
+    .then(data => res.send(data));
+});
+
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
 });
